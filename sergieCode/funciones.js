a0a7d8090722c82d4b8b7d1a8e5ec2ba26e2bdd8
myllerdev <myllerx3m@gmail.com>
2025-02-05 14:01:55 -0500

    creando una función para pasar de fahrenheit a celcius

--- sergieCode/funciones.js
+++ sergieCode/funciones.js
@@ -6,6 +6,15 @@ function suma(x,y){
 }
 
 let respuesta = suma(a,b)
-console.log(`La respuesta de ${a} + ${b} es igual a:`, respuesta
-    
-)
+console.log(`La respuesta de ${a} + ${b} es igual a:`, respuesta)
+
+//PASANDO DE FAHRENHEIT A CELCIUS
+
+const temperaturaF = 76
+
+function transformarACelcios(FAHRENHEIT){
+    return (FAHRENHEIT-32) * (5/9)
+}
+
+const respuesta2 = transformarACelcios(temperaturaF)
+console.log(`El equivalente de ${temperaturaF} en Celcius es: ${parseInt(respuesta2)}`)
