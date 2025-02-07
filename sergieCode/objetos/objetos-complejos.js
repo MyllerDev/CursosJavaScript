556077e6df01490802495b2dcdc3d57a16c4d7cd
myllerdev <myllerx3m@gmail.com>
2025-02-06 23:56:16 -0500

    viendo y creando archivos sobre objetos literares en js

---
+++ sergieCode/objetos/objetos-complejos.js
@@ -0,0 +1,14 @@
+
+
+//TAMBIEN SE PUEDEN PODER ARREGLOS Y OBJETOS DENTRO DE LOS MISMOS OBJETOS ASÍ:
+
+const vendedor = {
+    nombre: 'pacho',
+    empresa: 'Bimbo',
+    Habilidades: ['carisma', 'puntualidad'],
+    //tambien se pueden poner fucniones asi:
+    vender: function(){
+        console.log('Pedro a vendido un auto')
+    }
+
+}
---
+++ sergieCode/objetos/objetos.js
@@ -0,0 +1,20 @@
+
+//Esto es un objeto:
+//Son una estructura de datos que almacenan pares clave valor (Color:Rojo)
+let auto1 = {
+    marca: "Toyota",
+    color: 'Rojo',
+    peso : 490,
+    modelo : 'siete'
+}
+let auto2 = {
+    marca: "Mazda",
+    color: 'Negro',
+    peso : 520,
+    modelo : 'Cinco'
+}
+console.log('Auto 1 : ', auto1)
+console.log('Auto 2 : ', auto2)
+
+//Tambien se puede mostrar un solo valor del objeto asi:
+console.log('El color del auto 1 es:', auto1.color)
