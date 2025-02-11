b3b9269385bf125d92ff28772dd7fe224c9a210f
myllerdev <myllerx3m@gmail.com>
2025-02-10 22:52:31 -0500

    Calculadora con todos los botones totalmente funcionables

---
+++ sergieCode/Calculadora/script.js
@@ -0,0 +1,16 @@
+//para agregar a la pantalla el valor del botón de la calculadora que toquemos
+function agregar(valor){
+    document.getElementById('pantalla').value += valor
+}
+
+//para borrar los datos visibles en la pantalla de la calculadora
+function borrar(valor){
+    document.getElementById('pantalla').value = ""
+}
+
+//realizar los calculos realizados en la pantalla
+function calcular (){
+    const valorPantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
